2b33d6f019c05b055b5757b71b648a44df09e239
ismalevjr <ismaellevino@icloud.com>
2025-01-18 16:45:30 -0300

    Continuação nos estudos de lógica de programação

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,6 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto =4;
+let numeroSecreto =9;
 console.log(`O número secreto é ${numeroSecreto}`)
 let chute  = prompt('Escolha um número entre 1 e 10');
 
