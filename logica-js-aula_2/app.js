3e485f62c3233490b147b3d4ea0db5316dd17903
ismalevjr <ismaellevino@icloud.com>
2025-01-21 21:20:34 -0300

    Continuação aula 2 lógica de programação

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,11 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
-}
+//enquanto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas}`);
+    } else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else{
+            alert (`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas +1;
+        tentativas ++;
+}break;
