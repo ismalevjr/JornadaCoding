107fcd2cb4bc3ac17ff033c3037ab71fa1776255
ismalevjr <ismaellevino@icloud.com>
2025-01-22 20:49:28 -0300

    Update do exercicio da aula

--- logica-js-aula_2/app.js
+++ logica-js-aula_2/app.js
@@ -1,16 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
+
 let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute;
-let tentativas = 1
+let tentativas = 1;
 
 //enquanto
 while (chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 10');
-
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas}`);
+        break;
     } else {
         if(chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
@@ -19,4 +19,10 @@ while (chute != numeroSecreto){
         }
         //tentativas = tentativas +1;
         tentativas ++;
-}break;
+}
+
+if(tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativas} tentativas`);
+}else{
+    alert(`Isso ai! Você descobriu o número secreto é ${numeroSecreto} com ${tentativa} tentativa`);
+}
