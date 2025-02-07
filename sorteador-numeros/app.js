0c8180dd423dcadff02b791aa82679f7faf69d56
ismalevjr <ismaellevino@icloud.com>
2025-02-07 08:51:20 -0300

    iniciando o modúlo de desafios

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear(){
+   let quantidadeDeNumeros = document.getElementById("quantidade").value;
+   let numeroMinimo = document.getElementById("de").value;
+   let  numeroMaximo = document.getElementById("ate").value;
+
+   alert(`quantidade: ${quantidadeDeNumeros}`);
+   alert(`Do número: ${numeroMinimo}`);
+   alert(`Até o número: ${numeroMaximo}`);
+}
