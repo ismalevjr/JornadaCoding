822b3c199858a3bafab75726f1c334082388bdef
ismalevjr <ismaellevino@icloud.com>
2025-02-08 08:40:17 -0300

    Concluído desafio numero sorteados

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,16 +1,53 @@
+
 function sortear(){
    let quantidadeDeNumeros = parseInt(document.getElementById("quantidade").value);
    let numeroMinimo = parseInt(document.getElementById("de").value);
    let  numeroMaximo = parseInt(document.getElementById("ate").value);
 
-   for (let i = 0 ; i < quantidadeDeNumeros; i++) {
-      let numeroSorteado = gerarNumeroAleatorio(numeroMinimo,numeroMaximo);
-      alert(numeroSorteado);
+   if ( quantidadeDeNumeros > numeroMaximo) {
+      alert("Verifique a quantidade de números, ela não pode ser maior que o número máximo")
+   }if(quantidadeDeNumeros > (numeroMaximo - numeroMinimo + 1 ) ) {
+      alert("A quantidade de números precisa caber dentro intervalo selecionado, verifique os números digitados")
+   }else{
+   let listaDeNumerosSorteados = [];
+   let numeroSorteado;
+
+   for (let i = 0; i < quantidadeDeNumeros; i++) {
+      numeroSorteado = gerarNumeroAleatorio(numeroMinimo,numeroMaximo);
+
+      while (listaDeNumerosSorteados.includes(numeroSorteado)) {
+         numeroSorteado = gerarNumeroAleatorio(numeroMinimo,numeroMaximo) ;
+      }
+
+      listaDeNumerosSorteados.push(numeroSorteado);
    }
+   let resultado = document.getElementById("resultado");
+   resultado.innerHTML = ` <label class="texto__paragrafo" id="numero_sorteado">Números sorteados:  ${listaDeNumerosSorteados}</label>`
+   alterarStatusBotaoReiniciar();
+}
 }
 
 
 function gerarNumeroAleatorio (min,max) {
-   return parseInt(Math.random()* (max - min) + min);
+   return Math.floor(Math.random()*(max-min+1))+min;
 }
 
+function alterarStatusBotaoReiniciar(){
+   let botaoReiniciar = document.getElementById("btn-reiniciar");
+   
+   if( botaoReiniciar.classList.contains("container__botao-desabilitado")){
+      botaoReiniciar.classList.remove("container__botao-desabilitado");
+      botaoReiniciar.classList.add("container__botao");
+   }else{
+      botaoReiniciar.classList.remove("container__botao");
+      botaoReiniciar.classList.add("container__botao-desabilitado");
+   }
+}
+
+function reiniciar (){
+   (document.getElementById("quantidade").value) = "";
+   (document.getElementById("de").value) = "";
+   (document.getElementById("ate").value) = "";
+   document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo" id="numero_sorteado">Números sorteados: nenhum até agora</label>';
+   alterarStatusBotaoReiniciar();
+}
