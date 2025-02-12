65199774c310cfa06369b941b6ef1344002b003d
ismalevjr <ismaellevino@icloud.com>
2025-02-11 22:17:00 -0300

    Continuação Desafio Carrinho de compras

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,29 +1,44 @@
+let valorTotalCarrinho = 0;
 function adicionar (){
-
-
-    let produtoSelecionado = document.getElementById("produto").value;
-    let listaDeProdutos = [];
-    let carrinhoDeCompras = 0;
-
-    let valorProduto;
-        switch (produtoSelecionado) {
-            case "Fone de ouvido - R$100":
-                    valorProduto = 100;
-                break;
-
-            case "Celular - R$1400":
-                    valorProduto = 1400;
-                break;
-            case "Oculus VR - R$5000":
-                    valorProduto = 5000;
-                break;
-        }
+    //Recuperar os valores: nome do produto, quantidade e valor
+    let produtoSelecionado = document.getElementById('produto').value;
+    let quantidadeDeProdutos = document.getElementById('quantidade').value;
+    let nomeProduto = produtoSelecionado.split('-')[0];
+    let valorUnitario = produtoSelecionado.split('R$')[1];
     
-    listaDeProdutos = produtoSelecionado;
-    carrinhoDeCompras = carrinhoDeCompras + valorProduto;
 
-    console.log(listaDeProdutos);
-    console.log(carrinhoDeCompras);
+    //Calcular o valor final do produto com base nas quantidades selecionadas.
+   let preco = valorUnitario * quantidadeDeProdutos;
+    // Adicionando produtos no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidadeDeProdutos}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>
+      </section>`;
 
+    // Atualizar o valor total
+
+    valorTotalCarrinho = valorTotalCarrinho + preco;
+
+    let totalGeralCarrinho = document.getElementById('valor-total');
+    totalGeralCarrinho.innerHTML = `<span class="texto-azul" id="valor-total">R$${valorTotalCarrinho}</span>
+      </p>`;
+    
+}
+
+function limpar(){
+    let carrinho = document.getElementById('lista-produtos');
+    let totalGeralCarrinho = document.getElementById('valor-total');
+    let quantidadeDeProdutos = document.getElementById('quantidade');
+
+    totalGeralCarrinho.innerHTML =`<span class="texto-azul" id="valor-total">R$${0}</span>
+      </p>`;
+    carrinho.innerHTML = "";
+    quantidadeDeProdutos.innerHTML = `<input class="quantidade-input" id="quantidade" type="number" placeholder="100">`;
+
+    
 
 }
+
+
